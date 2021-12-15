12b726beeccdfacbadb8e2ff11fea254318b95e9
gustavopvilela <gustavofga2016@gmail.com>
2021-12-15 16:21:43 -0300

    finalmente um avanço sto feliz :)))

---
+++ src/carrinho/carrinho.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        require_once "conection.php";
+        mysqli_select_db($connect, $banco) or die("Erro na seleção do banco!");
+    
+        if(empty($_GET["produto"])){
+            echo "SELECIONA UM CARAI";
+        }
+        else{
+            foreach($_GET["produto"] as $produto){
+                //echo $produto;
+                
+                $sql = "INSERT INTO `teste`.`produto` (`produto`.`id`, `produto`.`product`) VALUES (null, '". $produto. "');";
+
+                if(mysqli_query($connect, $sql)){
+                    $msg = "Gravado com sucesso!";
+
+                    echo
+                    "<script type='text/javascript'>
+                        alert('$msg');
+                    </script>";
+                }
+                else{
+                    $msg = "Erro ao gravar!";
+
+                    echo 
+                    "<script type='text/javascript'>
+                        alert('$msg');
+                    </script>";
+                }
+            }
+        }
+    ?>
+</body>
+</html>
---
+++ src/carrinho/conection.php
@@ -0,0 +1,19 @@
+<?php
+    // dados para conexão
+    $servidor = 'localhost';    // servidor
+    $usuario = 'root';          // usuário
+    $senhabd = '1lovemyc4t!';              // senha
+    $banco = 'teste';       // database
+
+    // conexão com o banco
+    $connect = mysqli_connect($servidor, $usuario, $senhabd) or die("Erro na seleção do banco.");
+
+    /* para testar se o banco está conectando
+    
+        $connect = mysqli_connect($servidor, $usuario, $senhabd);
+        
+        if(!$connect){
+            die('Não conectou! - '. mysql_error());
+        }
+        echo 'Conectou!'; */
+?>
