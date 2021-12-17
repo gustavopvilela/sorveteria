aaa6c86e1978fb0454f93bf4fc36dda2effcfa32
gustavopvilela <gustavofga2016@gmail.com>
2021-12-17 15:29:52 -0300

    UM AVANÇO AMÉM SENHOR GLÓRIA A DEUS

--- src/pedido/carrinho/carrinho.php
+++ src/pedido/carrinho/carrinho.php
@@ -1,51 +1,24 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-        require_once "conection.php";
-        mysqli_select_db($connect, $banco) or die("Erro na seleção do banco!");
-    
-        if(empty($_GET["produto"])){
-            echo "Nennhum item selecionado";
-        }
-        else{
-            foreach($_GET["produto"] as $produto){
-                //echo $produto;
-                
-                $sql = "INSERT INTO `teste`.`produto` (`produto`.`id`, `produto`.`product`) VALUES (null, '". $produto. "');";
+<?php
+    if(isset($_GET["enviar"])){
+        echo "<table><tr>";
 
-                if(mysqli_query($connect, $sql)){
-                    $msg = "Gravado com sucesso!";
-
-                    echo
-                    "<script type='text/javascript'>
-                        alert('$msg');
-                    </script>";
-                }
-                else{
-                    $msg = "Erro ao gravar!";
-
-                    echo 
-                    "<script type='text/javascript'>
-                        alert('$msg');
-                    </script>";
-                }
-            }
+        
+        foreach($_GET["prod_id"] as $id){
+            echo "<td>". $id. "</td>";
         }
 
-        $query = "SELECT product FROM produto;";
-        $result_query = mysqli_query($connect, $query) or die('Erro na query: '. $query. ' '. mysqli_error());
+        echo "</tr><tr>";
 
-        while ($row = mysqli_fetch_array($result_query)){ 
-            print $row["product"]. "<br>"; 
+        foreach($_GET["prod_qtde"] as $qtde){
+            echo "<td>". $qtde. "</td>";
+        }
+        
+        echo "</tr><tr>";
+
+        foreach($_GET["prod_valor"] as $valor){
+            echo "<td>". $valor. "</td>";
         }
 
-    ?>
-</body>
-</html>
+        echo "</tr></table>";
+    }
+?>
